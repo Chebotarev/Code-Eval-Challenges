8c1d4d23f17ec91367303629034564d8ab3da90a
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-03-04 11:26:24 -0800

    Finished fizz_buzz.rb

---
+++ fizz_buzz.rb
@@ -0,0 +1,33 @@
+def fizz_buzz(input)
+  x, y, n = input.split(' ').map { |char| char.to_i }
+
+  final_numbers = [1]
+
+  (2..n).each do |num|
+    final_numbers.push(get_fb_strings(x, y, num))
+  end
+
+   final_numbers.join(' ')
+end
+
+def get_fb_strings(x, y, num)
+  if num % x == 0
+    if num % y == 0
+      "FB"
+    else
+      "F"
+    end
+  else
+    if num % y == 0
+      "B"
+    else
+      num 
+    end
+  end
+end
+
+input_lines = File.readlines ARGV[0]
+
+input_lines.each do |input|
+    puts fizz_buzz(input)
+end
