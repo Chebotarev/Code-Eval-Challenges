44ad60dcd7e02641f6a2c99ae95e4d67e07040af
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-03-16 22:52:39 -0700

    Refactored Bit Positions

--- bit_positions.rb
+++ bit_positions.rb
@@ -1,6 +1,5 @@
 def bit_positions(n, p1, p2)
-  n_binary_array = n.to_s(2).reverse.split()
-  n_binary_array[p1-1] == n_binary_array[p2-1]
+  n[p1-1] == n[p2-1]
 end
 
 input_lines = File.readlines ARGV[0]
